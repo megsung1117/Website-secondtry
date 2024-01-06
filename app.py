8719337aaa28c32da5c1792a21e68e8455f493c7
megsung1117 <155562603+megsung1117@users.noreply.github.com>
2024-01-06 12:26:41 +0000

    Add html and css

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route('/')
 def index():
-  return 'hello world'
-    #return render_template('index.html')
+    return render_template('home.html')
   
 
 if __name__ == '__main__':
