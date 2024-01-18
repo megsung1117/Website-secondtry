2ec7b619ce0bda869d6c020d7b0e6a1f418561d5
megsung1117 <155562603+megsung1117@users.noreply.github.com>
2024-01-18 13:16:09 +0000

    update requirement text file

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-conn = sqlite3.connect('mydatabase.db')
-cursor = conn.cursor()
+# conn = sqlite3.connect('mydatabase.db')
+# cursor = conn.cursor()
 
 # cursor.execute("UPDATE jobs SET requirements = 'The core concept of i-Buzz is a data-driven marketing strategy, developed through online corpus observation and mash-up data research to provide customers with in-depth data analysis results. In addition, through self-operated digital products, we want to mine user behavior data and provide a better product experience ' WHERE id=2")
 
@@ -11,11 +11,11 @@ cursor = conn.cursor()
 
 # cursor.execute("UPDATE jobs SET requirements ='1. Analyze, design and implement systems and software, sometimes debugging.2. Skilled with Angular 10 or above.3. Familiar with Restful API integration is a must.4. Work with the engineers to solve frontend issues.' WHERE id = 4")
 
-# conn.commit()
-cursor.execute("SELECT * FROM jobs ")
-rows = cursor.fetchall()
-print(cursor)
-conn.close()
+# # conn.commit()
+# cursor.execute("SELECT * FROM jobs ")
+# rows = cursor.fetchall()
+# print(cursor)
+# conn.close()
 
 
 def job_upload_from_db():
